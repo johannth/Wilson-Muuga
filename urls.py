2a77cab4706794fa8b9be465a7fbe7aa881ef9ef
Jóhann Þ. Bergþórsson <johann.thorvaldur@gmail.com>
2010-12-11 17:13:39 +0000

    Initial commit

---
+++ urls.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2008 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.conf.urls.defaults import *
+from django.contrib import admin
+
+admin.autodiscover()
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+urlpatterns = patterns('',
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'index.html'}),
+    (r'big-dawg$', 'django.views.generic.simple.direct_to_template', {'template': 'bigdawg.html'})
+
+    # Uncomment this for admin:
+    (r'^admin/', include(admin.site.urls)),
+)
